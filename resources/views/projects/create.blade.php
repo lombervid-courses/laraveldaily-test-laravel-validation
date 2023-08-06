0ccd650227a7fe45e4a0ab8f69d0e6ac89e867a1
David Lombera <david.lomberlan@gmail.com>
2023-08-06 14:50:06 -0600

    complete tasks

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -8,11 +8,17 @@
     @csrf
     Title:
     <br />
-    <input type="text" name="title" />
+    <input type="text" name="name" />
+    @error('name')
+        <p><small>{{ $message }}</small></p>
+    @enderror
     <br /><br />
     Description:
     <br />
     <input type="text" name="description" />
+    @error('description')
+        <p><small>{{ $message }}</small></p>
+    @enderror
     <br /><br />
     <button type="submit">Save</button>
 </form>
